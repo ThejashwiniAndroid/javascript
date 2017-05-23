92741a2e168ef3571460d202cef318f8ec602c22
ThejashwiniAndroid <thejashwiniramakrishna3@gmail.com>
2017-05-23 18:17:08 +0530

    formsubfetch

---
+++ fsubfet.js
@@ -0,0 +1,36 @@
+function fetch()
+{
+    let name1= document.details.elements.firstname.value;
+
+    let name2= document.details.elements.lastname.value;
+    let pswd= document.details.elements.pwd.value;
+    let mob= document.details.elements.number.value;
+    let email= document.details.elements.email.value;
+    let gender= document.details.elements.gender.value;
+    let dob= document.details.elements.bday.value;
+    let cf_pwd = document.details.elements.cpwd.value;
+    
+    console.log("first name is "+name1);
+    console.log("last name is "+name2);
+    console.log("password is "+pswd);
+    console.log("Mobile number is "+mob);
+    console.log("email-id is "+email);
+    console.log("Gender is "+gender);
+    console.log("dob is "+dob);
+    
+}
+
+function CheckPassword()   
+{   
+var passw= document.details.elements.pwd.value ; 
+var cpassw= document.details.elements.cpwd.value ; 
+if(passw!=cpassw)  
+{     
+alert("password not matching");
+return false;
+}
+else
+{
+    fetch();
+}
+}  
